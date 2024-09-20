45b937ddd1087c598e575a77cfff5c0f74e79549
Oyelaja Hussein <premiumitng@gmail.com>
2024-09-20 15:26:10 +0200

    Formatted the file

--- api/src/routers/future-meals.js
+++ api/src/routers/future-meals.js
@@ -1,13 +1,11 @@
 import express from "express";
 import knex from "../database_client.js";
 
-// This router can be deleted once you add your own router
 const futureMealsRouter = express.Router();
 
 futureMealsRouter.get("/", async (req, res) => {
   const futureMeals = await knex("meal").where("when", ">", knex.fn.now());
-  res.json(futureMeals); 
-}
-);
+  res.json(futureMeals);
+});
 
 export default futureMealsRouter;
