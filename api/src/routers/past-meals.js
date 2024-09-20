0036262960ce032c7dfa371bc24f43dbc1a6cfb0
Oyelaja Hussein <premiumitng@gmail.com>
2024-09-20 15:27:30 +0200

    Formatted the file

--- api/src/routers/past-meals.js
+++ api/src/routers/past-meals.js
@@ -1,15 +1,11 @@
 import express from "express";
-import knex from "../database_client.js"
+import knex from "../database_client.js";
 
-// This router can be deleted once you add your own router
 const pastMealsRouter = express.Router();
 
 pastMealsRouter.get("/", async (req, res) => {
-const pastMeals = await knex("meal").where("when", "<", knex.fn.now());
- res.json(pastMeals);
+  const pastMeals = await knex("meal").where("when", "<", knex.fn.now());
+  res.json(pastMeals);
 });
 
-
 export default pastMealsRouter;
-
-
