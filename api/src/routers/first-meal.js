77ea1c82f1e45dcf2b13dfd8184ed56ac66ad08d
Oyelaja Hussein <premiumitng@gmail.com>
2024-09-20 15:25:10 +0200

    Added 404 reponse

--- api/src/routers/first-meal.js
+++ api/src/routers/first-meal.js
@@ -1,12 +1,15 @@
 import express from "express";
-import knex from "../database_client.js"
+import knex from "../database_client.js";
 
-// This router can be deleted once you add your own router
 const firstMealRouter = express.Router();
 
 firstMealRouter.get("/", async (req, res) => {
-const firstMeal = await knex("meal").orderBy("id").first();
-  res.json(firstMeal);
+  try {
+    const firstMeal = await knex("meal").orderBy("id").first();
+    res.json(firstMeal);
+  } catch (error) {
+    res.status(404).json({ error: "No meal found" });
+  }
 });
 
 export default firstMealRouter;
