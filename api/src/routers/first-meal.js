cec9ff9dcace0d0b00270f326f9e486ffb304443
Oyelaja Hussein <premiumitng@gmail.com>
2024-09-30 22:14:54 +0200

    First Meal Uodated

--- api/src/routers/first-meal.js
+++ api/src/routers/first-meal.js
@@ -6,6 +6,7 @@ const firstMealRouter = express.Router();
 firstMealRouter.get("/", async (req, res) => {
   try {
     const firstMeal = await knex("meal").orderBy("id").first();
+    
     res.json(firstMeal);
   } catch (error) {
     res.status(404).json({ error: "No meal found" });
